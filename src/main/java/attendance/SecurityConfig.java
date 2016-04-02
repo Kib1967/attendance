ac9746b5a6d117ec9d2ab303535f2175244412be
Andy McKibbin <andymckibbin@yahoo.co.uk>
2016-04-03 11:03:30 +1200

    CSRF - not working

--- src/main/java/attendance/SecurityConfig.java
+++ src/main/java/attendance/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -37,5 +39,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	        .logout()
 	            .permitAll()
 	            .logoutSuccessUrl("/login?logout");
+        
+        // Obviously this is heinous. I don't know yet why the commented-out solution doesn't work
+        http.csrf().disable();
+            //.csrfTokenRepository(csrfTokenRepository());
+    }
+    
+    private CsrfTokenRepository csrfTokenRepository() 
+    { 
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository(); 
+        repository.setSessionAttributeName("_csrf");
+        return repository; 
     }
 }
